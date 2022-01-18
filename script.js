8c92d53118cf6f5ac04fe3650f8415569a0ca78c
Adriel <carlos.adriel@hotmail.com>
2022-01-17 18:47:24 -0600

    Se cambio lo largo del juego, detecta cuando pierdes al tocar el blocke con el dinosaurio

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function saltar(){
     if (dino.classList != "animar"){
     dino.classList.add("animar");
     }
-    setTimeout(function(){
-        dino.classList.remove("animar")
-    },500);
+    setTimeout(() => { dino.classList.remove("animar") }, 500);
 }
+
+var muere = setInterval(() => {
+    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    var bloqueLeft = parseInt(window.getComputedStyle(bloque).getPropertyValue("left"));
+    if(bloqueLeft < 20 && bloqueLeft < 0 && dinoTop >= 130){
+        bloque.style.animation = "none";
+        bloque.style.display = "none"
+        alert("Perdiste");
+        location.reload();
+    }
+},10)
